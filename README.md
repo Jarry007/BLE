6fb2b24648d5faa3bb488df7197627b935aa35f8
Jarry007 <940615834@qq.com>
2019-11-05 17:33:35 +0800

    echarts相关

--- README.md
+++
@@ -1,7 +0,0 @@
-# 预览图
-
-<img src="pic/001.png" style="width:50%;height:50%">
-<img src="pic/002.png" style="width:50%;height:50%">
-<img src="pic/003.png" style="width:50%;height:50%">
-<img src="pic/004.png" style="width:50%;height:50%">
-
--- pages/bar/index.js
+++ pages/bar/index.js
@@ -1,130 +1,189 @@
 import * as echarts from '../../ec-canvas/echarts';
 
 const app = getApp();
-var data_ = []
-function addNum(){
-  var num = Math.random()*10+15
-  data_ = data_.concat([num])
-  return data_ 
+
+const randomData = function(){
+   const arry = new Array
+   for(let i=0;i<12;i++){
+      arry.push(Math.floor(Math.random() * 100))
+   }
+
+
+   return arry
+   
 }
+
 var chart = null;
 
 Page({
-  onShareAppMessage: function (res) {
-    return {
-      title: 'ECharts 可以在微信小程序中使用啦！',
-      path: '/pages/index/index',
-      success: function () { },
-      fail: function () { }
-    }
-  },
-  data: {
-    ec: {
-      lazyLoad: true
+   onShareAppMessage: function(res) {
+      return {
+         title: 'ECharts 可以在微信小程序中使用啦！',
+         path: '/pages/index/index',
+         success: function() {},
+         fail: function() {}
+      }
+   },
+   data: {
+      ec: {
+         lazyLoad: true
       },
-    temperature: [17.3, 20.4, 16.9, 15.0, 14.3, 13.2, 17.3, 20.4, 16.9,]
-    
-  },
-  onLoad:function(){
-    this.lineCharts = this.selectComponent('#mychart-line');
-    this.judge()
-    
-  },
-  judge(){
-    if(!chart){
-      this.initLine()
-    }else{
-      this.getData()
-    }
-  },
-  initLine(){
-    this.lineCharts.init((canvas, width, height)=>{
-       chart = echarts.init(canvas, null, {
-        width: width,
-        height: height
-      });
-      this.setOption(chart);
-      return chart
-    })
-  },
-  setOption: function (chart) {
-    chart.clear();  // 清除
-    chart.setOption(this.getData());  //获取新数据
-  },
-  getData(){
-    let option = {
-      title: {
-        text: '温度、湿度变化表',
-        left: 'center',
-      },
-      color: ["#FF6666", "#3366FF", "#FF9933"],
-      legend: {
-        data: ['温度', '湿度', '浓度'],
-        top: 20,
-        left: 'center',
-        z: 100
-      },
-      grid: {
-        containLabel: true
-      },
-      tooltip: {
-        show: true,
-        trigger: 'axis'
-      },
-      xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
-        // show: false
-      },
-      yAxis: {
-        x: 'center',
-        type: 'value',
-        axisLable: {
-          formatter: "{value} ℃"
-        },
-        splitLine: {
-          lineStyle: {
-            type: 'dashed'
-          }
-        }
-        // show: false
-      },
-      series: [{
-        name: '温度',
-        type: 'line',
-        smooth: true,
-        data: this.data.temperature,
-        markLine: {
-          symbol: "none",               //去掉警戒线最后面的箭头
-          label: {
-            position: "end"          //将警示值放在哪个位置，三个值“start”,"middle","end"  开始  中点 结束
-          },
-          data: [{
-            silent: false,             //鼠标悬停事件  true没有，false有
-            lineStyle: {               //警戒线的样式  ，虚实  颜色
-              type: "dotta",
-              color: "#FA3934",
+      x: ['10-1', '10-2', '10-3', '10-4', '10-5', '10-6', '10-7', '10-8', '10-9', '10-10', '10-11', '10-12',],
+      temperature: [17.3, 20.4, 16.9, 15.0, 14.3, 13.2, 17.3, 20.4, 16.9, ],
+      y:[25,24,43,45,43,14,53,21,35,25,62]
+
+   },
+   onLoad: function() {
+      this.lineCharts = this.selectComponent('#mychart-line');
+      this.judge()
+
+   },
+   judge() {
+      if (!chart) {
+         this.initLine()
+      } else {
+         this.setOption(chart)
+      }
+   },
+   initLine() {
+      this.lineCharts.init((canvas, width, height) => {
+         chart = echarts.init(canvas, null, {
+            width: width,
+            height: height
+         });
+         this.setOption(chart);
+         return chart
+      })
+   },
+   setOption(chart) {
+      // chart.clear(); // 清除
+      chart.setOption(this.getData()); //获取新数据
+   },
+   getData() {
+      let option = {
+
+
+         grid: {
+            containLabel: true
+         },
+         tooltip: {
+            show: true,
+            backgroundColor: 'rgba(0,0,0,0.5)',
+            borderRadius: 10,
+            trigger: 'axis',
+            formatter(params) {
+               const item = params[0]
+
+               return `${item.name}
+${item.data}度`
             },
-            yAxis: 11.75           // 警戒线的标注值，可以有多个yAxis,多条警示线   或者采用   {type : 'average', name: '平均值'}，type值有  max  min  average，分为最大，最小，平均值
-          }]
-        }
-      }, {
-        name: '湿度',
-        type: 'line',
-        smooth: true,
-        data: [15.1, 13.2, 17.3, 20.4, 16.9, 15.0, 14.3]
-      },
-      {
-        name: '浓度',
-        type: 'line',
-        smooth: true,
-        data: [16.1, 13.6, 11.3, 18.4, 18.9, 17.0, 18.2]
-      },]
-    }
-    return option
-  },
-  changeValue(){
-    
-  }
+            position: function (pos, params, dom, rect, size) {
+               // 鼠标在左侧时 tooltip 显示到右侧，鼠标在右侧时 tooltip 显示到左侧。
+              
+               var obj = { top: 60 };
+               // obj[['right','left'][+(pos[0] < size.viewSize[0] / 2)]] = pos[0]+5;
+               obj['left'] = pos[0] < size.viewSize[0] / 2?pos[0]+10:pos[0]-50
+               
+               return obj;
+            }
+         },
+
+         xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            axisTick: {
+               show: false
+            },
+            data: this.data.x,
+            // show: false
+         },
+         yAxis: {
+            x: 'center',
+            type: 'value',
+            axisLabel: {
+               show: false
+            },
+            axisLine: {
+               show: false
+            },
+            axisTick: {
+               show: false
+            },
+            splitLine: {
+               show: false
+            }
+
+         },
+
+         series: [{
+
+            type: 'line',
+            smooth: true,
+            symbol: 'none',
+            label: {
+
+            },
+            data: this.data.y,
+            itemStyle: {
+               normal: {
+                  lineStyle: {
+                     width: 0,
+                     color: 'blue'
+                  }
+               }
+            },
+            areaStyle: {
+               normal: {
+                  color: {
+                     type: 'linear',
+                     x: 0,
+                     y: 0,
+                     x2: 0,
+                     y2: 1,
+                     colorStops: [{
+                        offset: 0,
+                        color: '#7f8de6' // 0% 处的颜色
+
+                     }, 
+                        {offset: 0.5,
+                           color: '#a7afe2' // 0% 处的颜色
+
+                     }, {
+                        offset: 1,
+                        color: 'white' // 100% 处的颜色
+                     }],
+                  }
+
+               }
+            },
+
+         }]
+      }
+      return option
+   },
+
+   refresh() {
+     
+      this.setData({
+         active: !this.data.active
+      })
+
+      setTimeout(() => {
+         this.setData({
+            active: !this.data.active,
+            nowTime:new Date().toLocaleString()
+         })
+         
+      }, 500)
+      this.getlineData()
+
+   },
+   getlineData() {
+      this.setData({
+         y: randomData()
+
+      })
+      console.log(chart)
+      this.judge()
+   }
 });
